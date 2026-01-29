ab04132f0dea5559fae48d5a5facd7a3fdc33f81
omrajtambat261-creator <omrajtambat261@gmail.com>
2026-01-29 13:24:30 +0530

    new changes added

--- src/customHooks/useFetch.jsx
+++ src/customHooks/useFetch.jsx
@@ -2,20 +2,27 @@ import { useEffect, useState } from "react";
 
 function useFetch(url)
 {
-    let [loading, setLoading]=useState(false)
     let[data, setData]=useState(null)
-    async function fetchData()
-    {
-        setLoading(true)
-        let response=await fetch(url);
-        let responseData=await response.json()
-        setData(responseData)
-        setLoading(false)
-    }
+    // async function fetchData()
+    // {
+    //     setLoading(true)
+    //     let response=await fetch(url);
+    //     let responseData=await response.json()
+    //     setData(responseData)
+    //     setLoading(false)
+    // }
 
-    useEffect(()=>{fetchData()}, [url])
+    useEffect(()=>
+        {
+            async function fetchData() 
+            {
+                let response = await fetch(url);
+                let responseData=await response.json()
+                setData(responseData)
+            }
+            fetchData()}, [url])
 
-    return {loading,data}
+    return {data}
 }
 
 export default useFetch;
--- src/products/DisplayProduct.jsx
+++ src/products/DisplayProduct.jsx
@@ -16,7 +16,7 @@ export default function DisplayProduct(props) {
               <h5 className="card-title">Card title</h5>
               <p className='card-text'>{product.category}</p>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="https://instagram.com" className="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
